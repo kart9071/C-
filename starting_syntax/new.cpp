c63d56461382f032701da1c7d878aece740db3b2
kart9071 <karthikakala13@gamail.com>
2022-12-21 22:06:58 +0530

    new commit

--- starting_syntax/new.cpp
+++ starting_syntax/new.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"hello world";
-    cout<<"hello world";
+    cout<<"hello world"<<endl;
+    cout<<"hello world"<<endl;
+  
     return 0;
 }
---
+++ variables/variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int my_num=10;
+    cout<<"The number is: "<<my_num<<endl;
+    float my_float=10.0;
+    cout<<"the float variable is:"<<my_float<<endl;
+    double my_double=10.563823472;
+    cout<<"the float variable is:"<<my_double<<endl;
+    string my_text="Hello world";
+    bool mybool=true;
+    cout<<my_text<<endl<<mybool;
+    return 0;
+}
